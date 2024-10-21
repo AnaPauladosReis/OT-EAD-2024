11777668f55611888d10dde4377f37ca8fb8b5fc
Ana Paula dos Reis <anareis87@icloud.com>
2024-10-21 20:30:02 -0300

    desafio-01-js

---
+++ DESAFIO-JS-01/desafio-01.js
@@ -0,0 +1,80 @@
+//Desafio 01 ✨ 
+// Declarar uma variável chamada `myvar`, sem valor.
+//R=
+let myvar;
+
+//₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊
+
+// Após declarada, atribua o valor 10 à variável `myvar`.
+//R=
+myvar = 10;
+//₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊
+
+// Declare uma nova variável chamada `soma`, e adicione uma instrução
+//somando os valores 15 e 8.
+//R=
+let soma = 15 + 8;
+//₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊
+
+// Atribua à variável `soma` todo o valor dela, somando 1, usando o operador
+//de soma abreviado.
+//R=
+soma +=1;
+//₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊
+
+// Atribua à variável `soma` todo o valor dela, multiplicando por 3, usando o
+//operador de multiplicação abreviado.
+//R=
+soma *=3;
+//₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊
+
+// Qual é o valor da variável `soma` até aqui?
+//R=
+console.log(soma); //resultado 78
+//₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊
+
+// Declare uma variável chamada `unisenai`, atribuindo à ela o valor booleano
+//que representa `verdadeiro`.
+//R=
+let unisenai = true;
+//₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊
+
+// Declare uma variável chamada `comida` que recebe um array com os valores
+//arroz', 'feijão' e 'ovo'.
+//R=
+let comida = ["arroz", "feijão", "ovo"];
+//₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊
+
+// Digite a instrução que imprime o valor de 'feijao', que está na variável
+//`comida`.
+//R=
+console.log(comida[1]);
+//₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊
+
+// Digite o código que verifica se a variável `soma' é igual a variável `myvar`
+//(testando também o tipo).
+//R=
+console.log(soma === myvar); //false
+//₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊
+
+// Digite o código que verifica se a variável `myvar` é menor ou igual à variável
+//`soma`.
+//R=
+console.log(myvar <= soma); //true
+
+//₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊
+
+// Crie uma função chamada `divisao` que receba como parâmetro dois
+//números, e retorne o resultado da divisão entre eles.
+//R=
+function calcularDivisao(num1, num2) { 
+  let resultado = num1 / num2;
+  alert(`O resultado da divisão de ${num1} por ${num2} é: ${resultado}`);
+}
+//₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊
+
+// Invoque a função criada acima, passando os parâmetros 10 e 2.
+//R=
+calcularDivisao(10, 2);
+
+//₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊₊˚ ‿︵‿︵‿︵୨୧ · · ♡ · · ୨୧‿︵‿︵‿︵ ˚₊
